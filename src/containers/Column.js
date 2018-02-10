bec06d77cb8aaec80700397577da37b9c12769a2
Gideon Baik <gideonbaik@gmail.com>
2018-02-10 14:41:22 -0800

    Add add blank card functionality

--- src/containers/Column.js
+++ src/containers/Column.js
@@ -9,24 +9,33 @@ class Column extends Component {
   constructor(props) { 
     super(props);
 
+    this.state = {
+      data: ['']
+    }
+
     this.handleAddingCard = this.handleAddingCard.bind(this);
   }
 
   handleAddingCard() {  
-    const { dispatch } = this.props;
+    // const { dispatch } = this.props;
 
-    cardActions.ADD_NEW_CARD(dispatch);
+    // cardActions.ADD_NEW_CARD(dispatch);
+  
+    const { data } = this.state;
+
+    this.setState({
+      data: [...data, 'Blank']
+    })  
   }
   
   render() {
-    const { title, card1, card2 } = this.props;
-    const test = ['hello', 'world', 'bye', '', '', ''];
+    const { title } = this.props;
 
     return (
       <div>
         <h1>{title}</h1>
-        {test.map((test, id) => (
-          <Card data={test} key={id}/>
+        {this.state.data.map((data, id) => (
+          <Card data={data} key={id}/>
         ))}
         <button onClick={this.handleAddingCard}>+ Add a card</button>
       </div>
